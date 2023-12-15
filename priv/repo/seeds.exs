dc7147f747e5c5776da0a68e56ca89da8f2d215c
Bryan Anthonio <dev@bryananthonio.com>
2023-12-14 17:05:39 -0800

    chore: add script to seed data into database

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias StoicQuotes.Quotes
+quotes_path = "priv/repo/quotes.json"
+
+quotes_path
+|> File.read!()
+|> Jason.decode!()
+|> Enum.each(fn attrs ->
+  quote = %{quote: attrs["quote"], author: attrs["author"], source: attrs["source"]}
+
+  case Quotes.create_quote(quote) do
+    {:ok, _quote} ->
+      :ok
+
+    {:error, _changeset} ->
+      :duplicate
+  end
+end)
