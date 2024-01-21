a14a08f9b5cf42815d4c0b5df37708676ed55aa0
Bryan Anthonio <dev@bryananthonio.com>
2024-01-20 18:25:26 -0800

    chore: update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule StoicQuotes.MixProject do
     [
       app: :stoic_quotes,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
